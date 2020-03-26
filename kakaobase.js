e22dd997de3be2a325054d5fa663fb0de5546fb8
delta-kor <aidenkor11@gmail.com>
2020-03-26 11:35:20 +0900

    Added SQLiteDatabase reference, method loadDatabase

--- kakaobase.js
+++ kakaobase.js
@@ -1,5 +1,19 @@
 // kakaobase.js
 importPackage(java.lang);
+importPackage(android.database.sqlite);
+
+/**
+ * @typedef {Object} SQLiteDatabase
+ * @property {function(string):Cursor} rawQuery
+ * @property {function(string):SQLiteDatabase,null,number} openDatabase
+ * @link https://developer.android.com/reference/android/database/sqlite/SQLiteDatabase
+ */
+
+/**
+ * @typedef {Object} Cursor
+ * @property {function():number} getColumnCount
+ * @link https://developer.android.com/reference/android/database/Cursor
+ */
 
 /**
  * Kakaobase
@@ -9,8 +23,15 @@ importPackage(java.lang);
 const Kakaobase = (function() {
 
     /**
+     * Kakaotalk database location
+     * @type {string}
+     */
+    const databaseLocation = '/data/data/com.kakao.talk/databases';
+
+    /**
      * Initializes a new instance of Kakaobase
      * @constructs Kakaobase
+     * @property {?SQLiteDatabase} masterDatabase
      */
     function Kakaobase() {
         this.masterDatabase = null;
@@ -20,11 +41,21 @@ const Kakaobase = (function() {
     /**
      * Grant superuser permission
      * @name Kakaobase#grantPermission
-     * @type void
+     * @return void
      */
     Kakaobase.prototype.grantPermission = function() {
-        const process = Runtime.getRuntime().exec('su -c "chmod -R 777 /data/data/com.kakao.talk/databases"');
+        const process = Runtime.getRuntime().exec('su -c "chmod -R 777 ' + databaseLocation + '"');
         process.waitFor();
+    };
+
+    /**
+     * Load master, secondary database
+     * @name Kakaobase#loadDatabase
+     * @return string
+     */
+    Kakaobase.prototype.loadDatabase = function() {
+        this.masterDatabase = SQLiteDatabase.openDatabase(databaseLocation, null, 1);
+        this.secondaryDatabase = SQLiteDatabase.openDatabase(databaseLocation, null, 1);
     };
 
     return Kakaobase;
