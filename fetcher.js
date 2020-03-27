09cc9e36232a3b18c57cff0acd55f7229a8580b3
delta-kor <aidenkor11@gmail.com>
2020-03-27 11:36:07 +0900

    Added fetcher.js

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+//fetcher.js
+const $ = require('kakaobase').load();
+
+/**
+ * Fetcher
+ * @class
+ * @type {Fetcher}
+ */
+const Fetcher = (function() {
+
+    /**
+     * Initializes a new instance of Fetcher
+     * @constructs Fetcher
+     */
+    function Fetcher() {
+
+    }
+
+    return Fetcher;
+
+})();
